2d8c39e1c7e18879d75f6920891f91a97c101d25
LeaMiage <lea.cheoux@gmail.com>
2015-06-08 19:15:46 +0200

    Fixing rule editor for check before and check after

--- app/views/rule_forms/check_after.php
+++ app/views/rule_forms/check_after.php
@@ -1,2 +1,6 @@
-<label for="input_1">Check <input type="text" name="input_1" id="input_1" class="little-input" /></label>
-<label for="input_2">After <input type="text" name="input_2" id="input_2" class="little-input" /></label>
+<fieldset>
+    <label for="input_1">Check <input type="text" name="input_1" id="input_1" class="little-input" <?=$edit ? 'value="' . $rule_content[0] . '"' : '' ?>/></label>
+</fieldset>
+<fieldset>
+    <label for="input_2">After <input type="text" name="input_2" id="input_2" class="little-input" <?=$edit ? 'value="' . $rule_content[1] . '"' : '' ?>/></label>
+</fieldset>
