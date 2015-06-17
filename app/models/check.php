7e0fff8358c7ffb1d7414270d0c3cc7b124e11b5
LeaMiage <lea.cheoux@gmail.com>
2015-06-17 15:16:37 +0200

    Use production repo for check view

--- app/models/check.php
+++ app/models/check.php
@@ -16,13 +16,13 @@ $ruletypes_selector = Utils::getHtmlSelectOptions(
                                 true
                             );
 
-$codes = $code_key = Code::getCodeList($locale, RULES_PRODUCTION);
+$codes = $code_key = Code::getCodeList($locale, RULES_STAGING);
 reset($code_key);
 $code_key = key($code_key);
 $code_selector = Utils::getHtmlSelectOptions($codes, $code_key, true);
 
 $ruletypes = Rule::getRulesTypeList();
 $first_rule = array_values($rules)[0];
-$rules = Rule::getArrayRules($code_key, $locale, RULES_PRODUCTION);
+$rules = Rule::getArrayRules($code_key, $locale, RULES_STAGING);
 
-$rule_exceptions = RuleException::getArrayExceptions($code_key, $locale, RULES_PRODUCTION);
+$rule_exceptions = RuleException::getArrayExceptions($code_key, $locale, RULES_STAGING);
