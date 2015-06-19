42b229b9985105666717dea60f7200f2a023b164
TheoChevalier <theo.chevalier11@gmail.com>
2015-06-19 18:29:08 +0200

    Fixing typography in user guide

--- app/views/user-guide.php
+++ app/views/user-guide.php
@@ -12,7 +12,7 @@
 							<li><a href="#replace-with-rule">Replace with rule</a></li>
 							<li><a href="#plural-separator-rule">Plural separator rule</a></li>
 							<li><a href="#ignore-variable-rule">Ignore variable rule</a></li>
-							<li><a href="#quotation-marks-rule">Quotation mark rule</a></li>
+							<li><a href="#quotation-marks-rule">Quotation marks rule</a></li>
 							<li><a href="#check-before-rule">Check before rule</a></li>
 							<li><a href="#check-after-rule">Check after rule</a></li>
 						</ul>
@@ -30,7 +30,7 @@
 	<h2>Rules editor (Adding a new rule)</h2>
 
 		<div id="replace-with-rule">
-			<h3>"Replace [...] with [...]" rule : </h3>
+			<h3>“Replace [...] with [...]” rule: </h3>
 				<p class="user-guide-content">
 					For this rule type, you have to enter after “Replace” the character or string you want to search and be replaced in your text. Then, you have to enter after “with” the substitution character or string.
 					<br/><br/>
@@ -42,12 +42,12 @@
 		<br/><br/>
 
 		<div id="plural-separator-rule">
-			<h3>"Plural separator [...]" rule : </h3>
+			<h3>“Plural separator [...]” rule: </h3>
 				<p class="user-guide-content">
-					This rule type is dedicated to translators who use specific character to separate strings and plural strings in traduction files.<br/>
-					For instance : Fenêtre partagée avec #1 onglet;Fenêtres partagées avec #1 onglets
+					This rule type is dedicated to localization formats relying on specific character to separate plurals in a string.<br/>
+					For instance: “Fenêtre partagée avec #1 onglet;Fenêtres partagées avec #1 onglets”
 					<br/><br/>
-					Example in Typolib' : <br/>
+					Example in Typolib': <br/>
 					<img src="/img/user-guide-screens/plural_separator.PNG" alt="Plural separator rule example" height="" width="400px" border=1 border-color="white" class="user-guide-image">
 				</p>
 		</div>
@@ -55,11 +55,11 @@
 		<br/><br/>
 
 		<div id="ignore-variable-rule">
-			<h3>"Ignore variable [...]" rule : </h3>
+			<h3>"Ignore variable [...]” rule: </h3>
 				<p class="user-guide-content">
 					For this rule type, you have to enter variables to ignore which are used in your files.
 					<br/><br/>
-					Example in Typolib' : <br/>
+					Example in Typolib’: <br/>
 					<img src="/img/user-guide-screens/ignore_variable.PNG" alt="Ignore variable rule example" height="" width="400px" border=1 border-color="white" class="user-guide-image">
 				</p>
 		</div>
@@ -67,12 +67,12 @@
 		<br/><br/>
 
 		<div id="quotation-marks-rule">
-			<h3>"Quotation mark [...] [...]" rule : </h3>
+			<h3>"Quotation mark [...] [...]” rule: </h3>
 				<p class="user-guide-content">
 					For this rule type, you have to enter the opening and the closing quotation mark to use.<br/>
 					You don’t have to enter spaces after the opening quotation mark and before the closing quotation mark.
 					<br/><br/>
-					Example in Typolib' : <br/>
+					Example in Typolib’: <br/>
 					<img src="/img/user-guide-screens/quotation_marks.PNG" alt="Quotation marks rule example" height="" width="400px" border=1 border-color="white" class="user-guide-image">
 				</p>
 		</div>
@@ -80,12 +80,12 @@
 		<br/><br/>
 
 		<div id="check-before-rule">
-			<h3>"Check [...] before [...]" rule : </h3>
+			<h3>“Check [...] before [...]” rule: </h3>
 				<p class="user-guide-content">
-					For this rule type, you have to enter 2 characters : 1 character which has to be placed before 1 other character.<br/>
-					Hint : you can drag and drop special characters in inputs (see <a href="#special-characters">Special characters</a> for more information).
+					For this rule type, you have to enter 2 characters: 1 character which has to be placed before 1 other character.<br/>
+					Hint: you can drag and drop special characters in inputs (see <a href="#special-characters">Special characters</a> for more information).
 					<br/><br/>
-					Example in Typolib' : <br/>
+					Example in Typolib’: <br/>
 					<img src="/img/user-guide-screens/check_before.PNG" alt="Check before rule example" height="" width="400px" border=1 border-color="white" class="user-guide-image">
 				</p>
 		</div>
@@ -93,22 +93,22 @@
 		<br/><br/>
 
 		<div id="check-after-rule">
-			<h3>"Check [...] after[...]" rule : </h3>
+			<h3>“Check [...] after[...]” rule: </h3>
 				<p class="user-guide-content">
-					For this rule type, you have to enter 2 characters : 1 character which has to be placed after 1 other character.
-					Hint : you can drag and drop special characters in inputs
+					For this rule type, you have to enter 2 characters: 1 character which has to be placed after 1 other character.
+					Hint: you can drag and drop special characters in inputs
 					<br/><br/>
-					Example in Typolib' : <br/>
+					Example in Typolib’: <br/>
 
 					<img src="/img/user-guide-screens/check_after.PNG" alt="Check after rule example" height="" width="400px" border=1 border-color="white" class="user-guide-image">
 
 					<br/>
 
-					For this specific example, the "white-space" special character has been dragged and dropped in the first input but it is not visible here.
+					For this specific example, the “white-space” special character has been dragged and dropped in the first input but it is not visible here.
 
 					<br/><br />
 
-					You can also add (it is optional) a comment to a rule type : <br/><br/>
+					You can also add (it is optional) a comment to a rule type: <br/><br/>
 
 					<img src="/img/user-guide-screens/comment_hint_description.PNG" alt="Comment hint description" height="" width="400px" border=1 border-color="white" class="user-guide-image">
 				</p>
@@ -134,7 +134,7 @@
 			<br/>
 
 			<p class="user-guide-content">
-				Once you have added a rule to a set of rules, you can :
+				Once you have added a rule to a set of rules, you can:
 				<ol>
 					<li>edit the rule itself</li>
 					<li>delete the rule itself</li>
@@ -159,7 +159,7 @@
 					<li>you choose a locale (the language)</li>
 					<li>you choose a specific set of rules you want to apply for the selected locale </li>
 					<li>you enter the text to check</li>
-					<li>the "corrected" text is displayed. All the rules of the set of rules have been checked and the errors have been corrected</li>
+					<li>the “corrected“ text is displayed. All the rules of the set of rules have been checked and the errors have been corrected</li>
 				</ol>
 			</p>
 	</div>
@@ -170,7 +170,7 @@
 		<h2>Special characters</h2>
 
 			<p class="user-guide-content">
-				In order to add rules to a set of rules, Typolib' can use specific characters : <br/><br/>
+				In order to add rules to a set of rules, Typolib’ can use specific characters: <br/><br/>
 
 					<img src="/img/user-guide-screens/empty_set_special_character.PNG" alt="Empty set special character" class="user-guide-image" style="display:inline-block">
 					<br/>
@@ -186,13 +186,13 @@
 
 					<img src="/img/user-guide-screens/white_space_special_character.PNG" alt="White space special character" class="user-guide-image" style="display:inline-block">
 					<br/>
-					This is the "standard" space character.
+					This is the “standard” space character.
 
 					<br/><br/>
 
 					<img src="/img/user-guide-screens/narrow_non_breaking_space_special_character.PNG" alt="Narrow no-break space special character" class="user-guide-image" style="display:inline-block">
 					<br/>
-					This is a non-breaking space but thiner. It is used in French for example (before " : ; ? ! » " and after " « ")
+					This is a non-breaking space but thinner. It is used in French for example (before “ ? ! ” )
 
 			</p>
 	</div>
