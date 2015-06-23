de3163e46655dca0ff458a3dfb402a95a6568390
TheoChevalier <theo.chevalier11@gmail.com>
2015-06-23 23:14:52 +0200

    Fixing exception deletion

--- app/models/api/deleting_exception.php
+++ app/models/api/deleting_exception.php
@@ -2,9 +2,8 @@
 namespace Typolib;
 
 $id_exception = $_GET['id_exception'];
-$id_rule = $_GET['id_rule'];
 
 echo '1';
 Utils::closeConnection();
 
-RuleException::manageException($code, $locale, $id_rule, 'delete', $id_exception);
+RuleException::manageException($code, $locale, $id_exception, 'delete');
